eae2602ee4344329b43b03f2f095fa93660bcb30
Michael Kevin <kevinadinata08@gmail.com>
2022-01-21 14:10:01 +0700

    add index page

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'portofolio',
+    title: 'Michael Kevin Adinata Portofolio',
     htmlAttrs: {
       lang: 'en'
     },
