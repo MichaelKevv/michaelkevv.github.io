d15f62a43e081b0029e906aa5a452bb64ec613b4
Michael Kevin <kevinadinata08@gmail.com>
2022-01-21 15:57:15 +0700

    add more page

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NavBar />
+    <main class="page-content">
+      <Nuxt />
+    </main>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<style>
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -14,17 +14,46 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
+    script: [
+      {
+        src: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      },
+      {
+        href: "https://use.fontawesome.com/releases/v5.15.4/js/all.js",
+        rel: "stylesheet"
+      }
+    ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        href: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+        rel: "stylesheet",
+        integrity: "sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3",
+        crossorigin: "anonymous"
+      },
+      {
+        href: "https://fonts.googleapis.com/css?family=Montserrat:400,700",
+        rel: "stylesheet"
+      },
+      {
+        href: "https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic",
+        rel: "stylesheet"
+      },
+      {
+        href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css",
+        rel: "stylesheet"
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    "~/assets/css/main.css",
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/main.js', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -33,7 +62,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
+
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
